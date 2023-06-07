aa05ff6d8d4679a9b3dd75e7348d075f6c6257c6
nat-0813 <129187214+nat-0813@users.noreply.github.com>
2023-06-07 16:04:54 -0400

    changes commit
    node changes

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # task-tracker-v1 project
  task tracker project with node.js
-// PROCESS OF BUILDING APPLICATION
+ PROCESS OF BUILDING APPLICATION
 //  install packages needed: npm i express ,npm install body-parser
 //create variables for each package
 //parse request bodies
 //get all tasks from file using get method
+// use post method then readfile to read tasks txt file; if there is an error send an err status else parse data and push new task else send err message
+//use delete method
--- task.js
+++ task.js
@@ -1,26 +1,160 @@
+// Create a "close" button and append it to each list item
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+//created variables for each package
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const { error } = require('console');
 
+//creating instance for express-package we grab from node modules
 const app = express();
-const port = 3000;
 
-//Middle ware to parse JSON request bodies
-app.use(bodyParser.text());
+//creating port for server
+const port = 6000;
 
-//Endpoint to get all tasks from the file
-app.get('./tasks', (req,res)=>{
-  fs.readFile('tasks.text', 'utf8',(err,data) =>{
-if(err){
-    console.log(error);
-    res.sendStatus(200);
- } else {
+//middleware to parse request bodies as JSON
+app.use(bodyParser.json());
+
+// Endpoint to get all tasks from the file
+app.get('/tasks', (req, res) => {
+  fs.readFile('tasks.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
       res.json(JSON.parse(data));
     }
   });
 });
 
-//Endpoint
+// Endpoint to add a new task to the file
+app.post('/tasks', (req, res) => {
+  const newTask = req.body;
+  // Validate the new task data
+  if (!newTask || !newTask.title || !newTask.description) {
+    res.sendStatus(400);
+    return;
+  }
+
+  fs.readFile('tasks.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      const tasks = JSON.parse(data);
+      tasks.push(newTask);
+      fs.writeFile('tasks.json', JSON.stringify(tasks), 'utf8', (err) => {
+        if (err) {
+          console.error(err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      });
+    }
+  });
+});
+
+// Endpoint to delete a task from the file
+app.delete('/tasks/:taskId', (req, res) => {
+  const taskId = req.params.taskId;
+
+  fs.readFile('tasks.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      const tasks = JSON.parse(data);
+      const updatedTasks = tasks.filter((task) => task.id !== taskId);
+      fs.writeFile('tasks.json', JSON.stringify(updatedTasks), 'utf8', (err) => {
+        if (err) {
+          console.error(err);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(200);
+        }
+      });
+    }
+  });
+});
+
+// Endpoint to update a task in the file
+app.put('/tasks/:taskId', (req, res) => {
+  const taskId = req.params.taskId;
+  const updatedTask = req.body;
+
+  fs.readFile('tasks.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      const tasks = JSON.parse(data);
+      const index = tasks.findIndex((task) => task.id === taskId);
+      if (index === -1) {
+        res.sendStatus(404);
+      } else {
+        tasks[index] = updatedTask;
+        fs.writeFile('tasks.json', JSON.stringify(tasks)
+
+
+
+
+
+
+
+
 
 
